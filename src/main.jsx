886299c82564ec48352350f33970844dd2119036
SeungGukYoo <dbtmd878@gmail.com>
2023-09-01 01:21:15 +0900

    feat: 에러 경로 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+
 import Issues from './components/Issues/Issues.jsx';
 import './index.css';
+
 import Description from './pages/Description/Description.jsx';
+import Error from './pages/Error/Error.jsx';
 import { store } from './store.js';
 
 const router = createBrowserRouter([
@@ -19,6 +22,14 @@ const router = createBrowserRouter([
       {
         path: '/:id',
         element: <Description />,
+      },
+      {
+        path: '/error',
+        element: <Error />,
+      },
+      {
+        path: '/*',
+        element: <Error />,
       },
     ],
   },
